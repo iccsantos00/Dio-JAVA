07447adf1b57ccf548b3d54862348dec62182ef0
iccsantos00 <isaccnv1@gmail.com>
2024-05-13 15:23:26 -0300

    Estruturas Condicionais

---
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+public class ResultadoEscolar {
+    public static void main(String[] args){
+        int nota = 9;
+        
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+        //OU
+
+        if(nota>=7)
+         System.out.println("Aprovado");
+
+        else if(nota >= 5 && nota<7)
+         System.out.println("Recuperação");
+
+        else
+         System.out.println("Reprovado");
+    }
+}
