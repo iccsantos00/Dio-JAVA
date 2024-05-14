b05e2cc4e8228e90ef03161890be17f6859b0feb
iccsantos00 <isaccnv1@gmail.com>
2024-05-14 17:28:26 -0300

    Ultimo desafio

---
+++ DesafiosEstruturas/src/ControleSimplesDeSaques.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        double limiteDiario = scanner.nextDouble();
+
+
+        for (int i = 1; ; i++) {
+            double valorSaque = scanner.nextDouble();
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break;
+            }
+            if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break;
+            }
+            limiteDiario -= valorSaque;
+            System.out.println("Saque realizado. Limite restante: " + limiteDiario);
+        }
+        scanner.close(); 
+    }
+}
