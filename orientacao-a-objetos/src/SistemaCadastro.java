a9994704ef28dbb4fed6cd02ad63a904e08a0c35
iccsantos00 <isaccnv1@gmail.com>
2024-05-20 14:29:06 -0300

    Construtores

---
+++ orientacao-a-objetos/src/SistemaCadastro.java
@@ -0,0 +1,8 @@
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa();
+        marcos.setEndereco("Rua das Marias");
+        System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+
+    }
+}
