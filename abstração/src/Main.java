d006c899717223f85e9c8d261f189990377ac0d1
iccsantos00 <isaccnv1@gmail.com>
2024-05-23 14:42:23 -0300

    Abstract

---
+++ abstração/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main extends ServicoMensagem {
+    
+
+    public void enviarMensagem(){
+        System.out.println("Enviar");
+    }
+    public void recceberMensagem(){
+        System.out.println("Receber");
+    }
+}
---
+++ abstração/src/ServicoMensagem.java
@@ -0,0 +1,7 @@
+
+public abstract class ServicoMensagem {
+       public abstract void enviarMensagem;
+       public abstract void receberMensagem; 
+}
+
+
